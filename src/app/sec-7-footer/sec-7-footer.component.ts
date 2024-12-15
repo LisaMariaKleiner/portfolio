5768742b378b7b2f3a42d319cb6d762f9fe5a261
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2024-12-15 11:49:58 +0100

    feat: Erstelle eine Footer Komponente von der es möglicch ist, ein externes routing zum Impressum oder der legal notice zu starten

--- src/app/sec-7-footer/sec-7-footer.component.ts
+++ src/app/sec-7-footer/sec-7-footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LegalNoticeDialogComponent } from '../legal-notice-dialog/legal-notice-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sec-7-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './sec-7-footer.component.html',
   styleUrl: './sec-7-footer.component.scss',
 })
@@ -14,7 +15,7 @@ export class Sec7FooterComponent {
 
   openLegalNoticeDialog(): void {
     this.dialog.open(LegalNoticeDialogComponent, {
-      width: '400px',
+      width: '100vw',
     });
   }
 }
