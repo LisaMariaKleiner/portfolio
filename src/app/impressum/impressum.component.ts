e79b4cb8354ac0d449fedf994717dfb72f511334
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2024-12-15 11:44:11 +0100

    feat: Erstelle eine Impressumsseite

---
+++ src/app/impressum/impressum.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Sec7FooterComponent } from '../sec-7-footer/sec-7-footer.component';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-impressum',
+  standalone: true,
+  imports: [Sec7FooterComponent, RouterLink],
+  templateUrl: './impressum.component.html',
+  styleUrl: './impressum.component.scss',
+})
+export class ImpressumComponent {}
