5b716f346ec4f826d9546ef3bb1dfa76b82ebbdf
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-01-15 12:59:42 +0100

    feat: Anpassungen für kleinere ENdgeräte

--- src/app/sec-1-startpage/sec-1-startpage.component.ts
+++ src/app/sec-1-startpage/sec-1-startpage.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sec-1-startpage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sec-1-startpage.component.html',
   styleUrl: './sec-1-startpage.component.scss',
 })
 export class Sec1StartpageComponent {
+  isMenuOpen = false;
   ngOnInit(): void {
     document.addEventListener('DOMContentLoaded', function () {
       const socialMediaButtons = document.querySelector(
@@ -33,5 +35,17 @@ export class Sec1StartpageComponent {
         observer.observe(socialMediaButtons);
       }
     });
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: UIEvent): void {
+    const windowWidth = (event.target as Window).innerWidth;
+    if (windowWidth > 1150 && this.isMenuOpen) {
+      this.isMenuOpen = false;
+    }
+  }
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    console.log('Menu open:', this.isMenuOpen);
   }
 }
--- src/app/sec-4-projects/sec-4-projects.component.ts
+++ src/app/sec-4-projects/sec-4-projects.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sec-4-projects',
   standalone: true,
   imports: [],
   templateUrl: './sec-4-projects.component.html',
-  styleUrl: './sec-4-projects.component.scss'
+  styleUrl: './sec-4-projects.component.scss',
 })
-export class Sec4ProjectsComponent {
+export class Sec4ProjectsComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
 
+  ngOnInit() {
+    this.muteAllMedia();
+  }
+
+  muteAllMedia() {
+    const audioElements = document.querySelectorAll('audio');
+    const videoElements = document.querySelectorAll('video');
+
+    audioElements.forEach((audio) => {
+      this.renderer.setProperty(audio, 'muted', true);
+    });
+
+    videoElements.forEach((video) => {
+      this.renderer.setProperty(video, 'muted', true);
+    });
+  }
 }
