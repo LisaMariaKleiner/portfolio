3da3729ac921a99a788ebac49c08bd7b18c2153a
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-01-22 11:26:17 +0100

    feat: Entferne RouterLink-Import aus der rechtlichen Hinweis-Komponente

--- src/app/legal-notice-dialog/legal-notice-dialog.component.ts
+++ src/app/legal-notice-dialog/legal-notice-dialog.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { Sec7FooterComponent } from '../sec-7-footer/sec-7-footer.component';
-import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-legal-notice-dialog',
   standalone: true,
-  imports: [Sec7FooterComponent, RouterLink, HeaderComponent],
+  imports: [Sec7FooterComponent, HeaderComponent],
   templateUrl: './legal-notice-dialog.component.html',
   styleUrl: './legal-notice-dialog.component.scss',
 })
