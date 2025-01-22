86d9257f37c8bb1b6421f00899a6eb591b869305
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-01-22 11:08:03 +0100

    feat: Füge Header-Komponente zum Impressum hinzu und verbessere das Layout

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Sec7FooterComponent } from '../sec-7-footer/sec-7-footer.component';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [Sec7FooterComponent, RouterLink],
+  imports: [Sec7FooterComponent, RouterLink, HeaderComponent],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss',
 })
