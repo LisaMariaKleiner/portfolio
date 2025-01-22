8fd534813c9736384c255f8c0d35438edd4428a1
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-01-22 11:10:14 +0100

    feat: Füge Header-Komponente zum rechtlichen Hinweis-Dialog hinzu

--- src/app/legal-notice-dialog/legal-notice-dialog.component.ts
+++ src/app/legal-notice-dialog/legal-notice-dialog.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Sec7FooterComponent } from '../sec-7-footer/sec-7-footer.component';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-legal-notice-dialog',
   standalone: true,
-  imports: [Sec7FooterComponent, RouterLink],
+  imports: [Sec7FooterComponent, RouterLink, HeaderComponent],
   templateUrl: './legal-notice-dialog.component.html',
   styleUrl: './legal-notice-dialog.component.scss',
 })
