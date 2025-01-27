685739ea1a5a1852d6afe65314d1ebb8c7ab501e
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-01-27 19:20:59 +0100

    feat: Integriere Übersetzungsdienst in die Projektkomponente und passe Inhalte an

--- src/app/sec-4-projects/sec-4-projects.component.ts
+++ src/app/sec-4-projects/sec-4-projects.component.ts
@@ -1,14 +1,26 @@
 import { Component, Renderer2, OnInit } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sec-4-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './sec-4-projects.component.html',
   styleUrl: './sec-4-projects.component.scss',
 })
 export class Sec4ProjectsComponent implements OnInit {
-  constructor(private renderer: Renderer2) {}
+  constructor(
+    private renderer: Renderer2,
+    private translate: TranslateService
+  ) {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
+
+  switchLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
 
   ngOnInit() {
     this.muteAllMedia();
