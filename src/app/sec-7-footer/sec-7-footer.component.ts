744efa0a997c166471e5c5b1dfe0470d429d0412
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-01-27 19:22:01 +0100

    feat: Integriere Übersetzungsdienst im Fußbereich und passe rechtliche Hinweise an

--- src/app/sec-7-footer/sec-7-footer.component.ts
+++ src/app/sec-7-footer/sec-7-footer.component.ts
@@ -1,20 +1,27 @@
 import { Component } from '@angular/core';
-import { LegalNoticeDialogComponent } from '../legal-notice-dialog/legal-notice-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
 import { RouterLink } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sec-7-footer',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, TranslateModule],
   templateUrl: './sec-7-footer.component.html',
   styleUrl: './sec-7-footer.component.scss',
 })
 export class Sec7FooterComponent {
-  constructor(private dialog: MatDialog) {}
+  constructor(private dialog: MatDialog, private translate: TranslateService) {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
+
+  switchLanguage(lang: string): void {
+    this.translate.use(lang);
+  }
 
   openLegalNotice() {
     // Öffnet die Route in einem neuen Tab oder Fenster
-    window.open('/legal-notice-dialog', '_blank', 'width=800,height=600');
+    window.open('/privacy-policy', '_blank', 'width=800,height=600');
   }
 }
