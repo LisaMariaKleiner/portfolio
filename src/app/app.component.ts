9b06f5e232f24ba0f9a05a6f7309609e7b9f0e9d
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-01-27 19:18:17 +0100

    feat: Bereinige App-Komponente durch Entfernen nicht verwendeter Importe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,14 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { Sec1StartpageComponent } from './sec-1-startpage/sec-1-startpage.component';
-import { Sec2DescriptionComponent } from './sec-2-description/sec-2-description.component';
-import { Sec3SkillsComponent } from './sec-3-skills/sec-3-skills.component';
-import { Sec4ProjectsComponent } from './sec-4-projects/sec-4-projects.component';
-import { Sec5OpinionsComponent } from './sec-5-opinions/sec-5-opinions.component';
-import { Sec6ContactComponent } from './sec-6-contact/sec-6-contact.component';
-import { Sec7FooterComponent } from './sec-7-footer/sec-7-footer.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet,
-    Sec1StartpageComponent,
-    Sec2DescriptionComponent,
-    Sec3SkillsComponent,
-    Sec4ProjectsComponent,
-    Sec5OpinionsComponent,
-    Sec6ContactComponent,
-    Sec7FooterComponent,
-    ReactiveFormsModule,
-  ],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Lisa Maria Kleiner';
