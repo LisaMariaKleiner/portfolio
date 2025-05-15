2d123eaf4c5c0337666ed5e52d9954a6f2f2bb53
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-05-15 13:31:39 +0200

    feat: aktualisiere die Datenschutzerklärung mit Übersetzung und verbessere das Layout

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -2,18 +2,19 @@ import { Component } from '@angular/core';
 import { Sec7FooterComponent } from '../sec-7-footer/sec-7-footer.component';
 import { HeaderComponent } from '../header/header.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { Nl2brPipe } from '../pipes/context.pipe';
 
 @Component({
-  selector: 'app-legal-notice-dialog',
+  selector: 'app-privacy-policy',
   standalone: true,
-  imports: [Sec7FooterComponent, HeaderComponent, TranslateModule],
+  imports: [Sec7FooterComponent, HeaderComponent, TranslateModule, Nl2brPipe],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss',
 })
 export class privacyPolicyComponent {
   constructor(private translate: TranslateService) {
-    translate.setDefaultLang('en');
-    translate.use('en');
+    translate.setDefaultLang('de');
+    translate.use('de');
   }
 
   switchLanguage(lang: string): void {
