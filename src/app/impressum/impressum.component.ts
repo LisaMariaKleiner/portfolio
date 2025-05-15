31e5e1c69d4526c85d6fb90e95135ef7bbb386be
Lisa Maria Kleiner <lisamaria.kleiner@icloud.com>
2025-05-15 13:30:37 +0200

    feat: übersetze Impressum-Inhalte und verbessere das Layout

--- src/app/impressum/impressum.component.ts
+++ src/app/impressum/impressum.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Sec7FooterComponent } from '../sec-7-footer/sec-7-footer.component';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-impressum',
   standalone: true,
-  imports: [Sec7FooterComponent, RouterLink, HeaderComponent],
+  imports: [Sec7FooterComponent, RouterLink, HeaderComponent, TranslateModule],
   templateUrl: './impressum.component.html',
   styleUrl: './impressum.component.scss',
 })
